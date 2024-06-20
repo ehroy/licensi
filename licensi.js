ac018486c1bd16315dd476c87dc76000913caf0e
ehroy <68073896+ehroy@users.noreply.github.com>
2024-06-20 15:08:19 +0700

    Update licensi.js

--- licensi.js
+++ licensi.js
@@ -1,3 +1,3 @@
 [
-  { code: 200, msg: 'yahaa mau ngapain', key: "U2FsdGVkX18EJvLtqXl6Clu93qqib/2vobusD4mdeUM=" }
+  { code: 200, msg: 'yahaa mau ngapain', key: "U2FsdGVkX18EJvLtqXl6Clu93qqib/2vobusD4mdeUM=" , mac: "00330-81471-84102-AA263"}
 ]
